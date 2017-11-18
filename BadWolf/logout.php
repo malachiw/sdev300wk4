403a8c3bd5939d3354b1b88e991210aa1161b376
Malachi Woodlee <malachi.woodlee@gmail.com>
2017-11-18 09:44:00 -0500

    Lots of changes. Should be turn in ready

--- BadWolf/logout.php
+++ BadWolf/logout.php
@@ -1,29 +1,15 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-   <title>Form Login</title>
+   <title>Doctor Who Logout</title>
 </head>
-
+<body>
 <?php
 session_start();
 unset($_SESSION['appusername']);
 unset($_SESSION['appemail']);
-
-
-// Display the Session information
-echo "<h3> Session Data after Logout  </h3>
-<table border='1'>
-<tr>
-        <td>Username </td>
-        <td> Email </td>
-      </tr>
-<tr>
-        <td>" . $_SESSION['appusername'] . "</td>" .
-        "<td>" . $_SESSION['appemail'] . "</td>
-      </tr>     
-    </table>";  
- 
- ?>    
+header("Location: loginAuth.html") 
+?>    
 
 </body>
 </html>
